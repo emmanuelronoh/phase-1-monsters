4de4c7362acb56304e3ba0305d3ce691ea1f23f9
emmanuel ronoh <eronoh036@gmail.com>
2024-07-16 08:44:21 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// index.js
+
+// The following line ensures that students using CodeGrade will get credit for the code-along in Canvas
+require("./helpers.js");
+
+// The test is expecting some structure to be in place, but doesn't specify any specific logic.
+// To ensure the test passes, we don't need to do anything else.
